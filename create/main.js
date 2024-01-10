4d59b458e9b411c78955862cfdff3cfd74b17f0e
rae-2 <cloudandelion@gmail.com>
2024-01-10 13:13:03 -0500

    cxbvd

--- create/main.js
+++ create/main.js
@@ -5,4 +5,18 @@ let newArr = []
 async function meow(API) {
   const api = "https://reverse1999-characters-kang-piscok.koyeb.app/api/v1/characters";
 
+  try {
+    const response = await fetch(api);
+    
+    if (!response.ok) {
+      throw new Error('erm error !!');
+    }
+    
+    const data = await response.json();
+    displayData(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
 }
+
+meow();
