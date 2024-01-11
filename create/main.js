921b76c6deccad19aab813e99da0542d80953a17
rae-2 <cloudandelion@gmail.com>
2024-01-11 12:56:14 -0500

    ok!

--- create/main.js
+++ create/main.js
@@ -4,19 +4,30 @@ let newArr = []
 
 async function meow(API) {
   const api = "https://reverse1999-characters-kang-piscok.koyeb.app/api/v1/characters";
+}
 
-  try {
-    const response = await fetch(api);
-    
-    if (!response.ok) {
-      throw new Error('erm error !!');
-    }
-    
-    const data = await response.json();
-    displayData(data);
-  } catch (error) {
-    console.error('Error fetching data:', error);
+try {
+  const response = await fetch(api);
+  
+  if (!response.ok) {
+    throw new Error('erm error !!');
   }
+  
+  const hehe = await response.json();
+  displayData(hehe);
+} catch (error) {
+  console.error('Error fetching data:', error);
+}
+
+
+function displayData(hehe) {
+  hehe.forEach((data) => {
+    const charInfo = document.createElement('div');
+
+    charInfo.innerHTML = `
+    
+    `
+  });
 }
 
 meow();
