f91288c6bebb6a73744d23b1f58063a3502189f2
rae-2 <cloudandelion@gmail.com>
2024-01-11 19:02:50 -0500

    what's gpog mon

--- create/js/main.js
+++ create/js/main.js
@@ -1,4 +1,17 @@
-import { images } from "./array";
+const images =[
+  six = 'https://static.wikia.nocookie.net/reverse1999/images/f/f1/6.png/revision/latest?cb=20231118210048', //six
+  tri = 'https://static.wikia.nocookie.net/reverse1999/images/8/87/37.png/revision/latest/scale-to-width-down/286?cb=20231019203459', //37
+  a_knight = 'https://static.wikia.nocookie.net/reverse1999/images/e/e1/A_Knight_Suitcase.png/revision/latest?cb=20231015180431', //a knight
+  apple = 'https://static.wikia.nocookie.net/reverse1999/images/f/fc/APPLe.png/revision/latest?cb=20231029183031', //apple
+  alient = 'https://static.wikia.nocookie.net/reverse1999/images/4/4f/AliEn_T.png/revision/latest/scale-to-width-down/350?cb=20231111141405', // alient t
+  ananlee = 'https://static.wikia.nocookie.net/reverse1999/images/6/64/An-an_Lee.png/revision/latest?cb=20231020001311', //an an lee
+  babyblue = 'https://static.wikia.nocookie.net/reverse1999/images/c/c6/Baby_Blue.png/revision/latest/scale-to-width-down/1000?cb=20231022043712', //baby blue
+  balloonparty = 'https://static.wikia.nocookie.net/reverse1999/images/b/bb/Balloon_Party_Chibi.png/revision/latest?cb=20231024175713', //balloon party
+  bette = 'https://static.wikia.nocookie.net/reverse1999/images/0/0d/Bette.png/revision/latest?cb=20231111164507', //bette
+  bkornblume = 'https://static.wikia.nocookie.net/reverse1999/images/a/ac/Bkornblume.png/revision/latest/scale-to-width-down/333?cb=20231025015231', //bkornblume
+
+];
+
 
 let newArr = [];
 
@@ -27,7 +40,7 @@ function displayData(hehe) {
     const charInfo = document.createElement('div');
 
     charInfo.innerHTML = `
-    
+
     `
   });
 }
