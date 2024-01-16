c5c7cbfb73b103ba56816f4d17f1a8cd70570fe2
rae-2 <cloudandelion@gmail.com>
2024-01-15 19:42:05 -0500

    eeeeee

--- create/js/array.js
+++ create/js/array.js
@@ -12,4 +12,31 @@ const images =[
 
 ];
 
+const outcomes = [
+    {
+        title: "Congratulations! You've collected ENDING 1.",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyEWInkLjBMrX-4ehYMqS8OXO5ihvrBa-JoA&usqp=CAU",
+        description: "Six was glad to pick apples with you!",
+    },
+    {
+        title2: "Congratulations! You've collected ENDING 2.",
+        image2: "https://media.tenor.com/Oas_7V6NajEAAAAM/laugh-point.gif",
+        description2: "37 was NOT interested in you. Go away lol",
+    },
+    {
+        title3: "Congratulations! You've collected ENDING 3.",
+        image3: "https://media.tenor.com/Oas_7V6NajEAAAAM/laugh-point.gif",
+        description3: "It seems A Knight doesn't want to talk to you.",
+    },
+    {
+        title4: "Congratulations! You've collected ENDING 4.",
+        image4: "https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/exploding-head.png",
+        description4: "why would you invite an APPLE to go APPLE PICKING?! APPLe air fried you and scattered your ashes under the apple tree!",
+    },
+    {
+        title5: "Congratulations! You've collected ENDING 4.",
+        image5: "https://www.snexplores.org/wp-content/uploads/2020/04/1030_LL_trees-1028x579.png",
+        description5: "AliEn T already picked all the apples... Should've been faster.",
+    },
+]
 export { images }
--- create/js/main.js
+++ create/js/main.js
@@ -1,17 +1,8 @@
-const images =[
-  'https://static.wikia.nocookie.net/reverse1999/images/f/f1/6.png/revision/latest?cb=20231118210048', //six
-  'https://static.wikia.nocookie.net/reverse1999/images/8/87/37.png/revision/latest/scale-to-width-down/286?cb=20231019203459', //37
-  'https://static.wikia.nocookie.net/reverse1999/images/e/e1/A_Knight_Suitcase.png/revision/latest?cb=20231015180431', //a knight
-  'https://static.wikia.nocookie.net/reverse1999/images/f/fc/APPLe.png/revision/latest?cb=20231029183031', //apple
-  'https://static.wikia.nocookie.net/reverse1999/images/4/4f/AliEn_T.png/revision/latest/scale-to-width-down/350?cb=20231111141405', // alient t
-  'https://static.wikia.nocookie.net/reverse1999/images/6/64/An-an_Lee.png/revision/latest?cb=20231020001311', //an an lee
-  'https://static.wikia.nocookie.net/reverse1999/images/c/c6/Baby_Blue.png/revision/latest/scale-to-width-down/1000?cb=20231022043712', //baby blue
-  'https://static.wikia.nocookie.net/reverse1999/images/b/bb/Balloon_Party_Chibi.png/revision/latest?cb=20231024175713', //balloon party
-  'https://static.wikia.nocookie.net/reverse1999/images/0/0d/Bette.png/revision/latest?cb=20231111164507', //bette
-  'https://static.wikia.nocookie.net/reverse1999/images/a/ac/Bkornblume.png/revision/latest/scale-to-width-down/333?cb=20231025015231', //bkornblume
+import { images } from "./array";
 
-];
+const outcomes = {
 
+}
 const DOMSelectors = {
   results: document.querySelector("#horhorhorhor"),
 }
