80c352865abec33efa81d108fede860530539c08
rae-2 <cloudandelion@gmail.com>
2024-01-16 12:43:10 -0500

    no more api lol

--- create/js/main.js
+++ create/js/main.js
@@ -10,29 +10,6 @@ const DOMSelectors = {
 let newArr = [];
 
 
-
-async function meow(API) {
-  const api = "https://reverse1999-characters-kang-piscok.koyeb.app/api/v1/characters";
-
-   try {
-    const response = await fetch(api);
-    
-    if (!response.ok) {
-      throw new Error('erm error !!');
-    }
-    
-    const data = await response.json();
-    displayData(data);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  } 
-}
-
-function displayData(data) {
-  
-}
-
-meow();
 //inserts the ending:
 
 /* function newstuff() {
